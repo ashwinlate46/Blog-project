b5c7a4982b07e407a2c88264bac5c40efba0aae9
Ashwin Late <ashwinlate46@gmail.com>
2024-09-22 09:12:04 +0530

    created blog project

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import AppContextProvider from './context/AppContext'
+import { BrowserRouter } from 'react-router-dom'
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  </BrowserRouter>
+
+
+
+)
